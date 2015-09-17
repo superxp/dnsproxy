a8f3f8827d9019aa07dcea0a608b6ac0bf643492
superxp <mrduanpeng@163.com>
2015-09-18 00:38:16 +0800

    what fuck

--- dnsproxy.js
+++ dnsproxy.js
@@ -1,6 +1,6 @@
 var dgram = require("dgram");
  
-const DNSADDRESS = "10.28.160.102";
+const DNSADDRESS = "8.8.8.8";
 const DNSPORT = "53";
  
 console.log("DNS Server: " + DNSADDRESS + ":" + DNSPORT);
@@ -13,19 +13,21 @@ var server  = dgram.createSocket("udp4", function (msg, rinfo) {
     client.send(msg, 0, msg.length, DNSPORT, DNSADDRESS);
     var tid = 0, buf = null;
     client.on("message", function (msg, rinfo) {
-		console.log(msg.toString('UTF-8', 0, msg.length))
-		var tbuf = new Buffer(msg,'Binary');
-		console.log(tbuf)
+		
+		
         buf = msg
         if (tid) clearTimeout(tid);
         tid = setTimeout(function () {
             tid = 0;
-			console.log("»º³åÇø³¤¶È:"+buf.length)
+			
 			console.log("QR:"+buf[0])
-			console.log("QR:"+tbuf[0])
 			console.log("AA:"+buf[5])
 			console.log("TC:"+buf[6])
 			console.log('RD:'+buf[7])
+			console.log('num:'+buf[12])
+			console.log('q:'+buf[13])
+			console.log('q:'+buf[16])
+			console.log('q:'+buf[17])
             server.send(buf, 0, buf.length, port, address);
             client.close();
         }, 15);
--- dnsproxy.py
+++ dnsproxy.py
@@ -16,24 +16,26 @@ udpSerSock.bind(ADDR)
 while True:
 	print 'wating for message...'
 	data, addr = udpSerSock.recvfrom(BUFSIZE)
+	print len(data)
+	print data
+	print 'QR:'+data[0]
+	if data[0]==0:
+	    print 'ask '
 	tdata = data[12:]
-	i = 1  
+	print len(tdata)
 	name = ''
-	
-    while True:
-	
-       d = ord(tdata[i])  
-       if d == 0:  
-          break;  
-       if d < 32:  
-          name = name + '.'  
-       else:  
-          name =  name + chr(d)  
-       i = i + 1  
-	
-	print name
-	
+    
+	for i in range(0,len(tdata)):
+	    d = ord(tdata[i])
+    	if d == 0: 
+    	   break;
+        if d < 32:
+    	   name = name + '.'   
+    	else:
+    	   name = name + tdata[i]
+    	
+		
+	print 'what fuck:'+name 
 	udpSerSock.sendto('[%s] %s'%(ctime(),data),addr)
 	print '...received from and retuned to:',addr
 
-udpSerSock.close()
